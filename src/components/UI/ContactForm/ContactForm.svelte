43c2a41ba74ac6159666d9fa82b8476bdb852ca8
David Hanlon <davidhanlon23@gmail.com>
2021-10-18 19:01:58 -0400

    add contact form

---
+++ src/components/UI/ContactForm/ContactForm.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import Button from '../Button/Button.svelte';
+
+    const inputClasses = "border border-gray-200 dark:border-gray-600 rounded-sm bg-white dark:bg-gray-900 text-black dark:text-white text-xl";
+    const labelClasses = "text-black dark:text-white text-xl font-normal mb-2"
+</script>
+<form>
+    <div class="flex mb-8">
+        <div class="flex flex-col mr-4">
+            <label for="contact-form-name" class={`${labelClasses}`}>Name</label>
+            <input id="contact-form-name" name="name" type="text" class={`${inputClasses} h-10 p-4`} required />
+        </div>
+        <div class="flex flex-col">
+            <label for="contact-form-email" class={`${labelClasses}`}>Email</label>
+            <input id="contact-form-email" name="name" type="text" class={`${inputClasses} h-10 p-4`} required />
+        </div>        
+    </div>
+    <div class="flex flex-col">
+        <label for="contact-form-message" class={`${labelClasses}`}>Message</label>
+        <textarea id="contact-form-message" name="name" type="text" class={`${inputClasses} p-4 h-56`} required />
+    </div>
+    <div class="mt-8 flex justify-center">
+        <Button classes="border-2 py-2 px-12 border-dh-secondary-dark-500 text-dh-secondary-dark-500 hover:text-white hover:bg-dh-secondary-dark-500 dark:hover:text-black" text="Submit" />
+    </div>
+</form>
+
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
+import ContactForm from '../components/UI/ContactForm/ContactForm.svelte';
 import Container from '../components/UI/Container/Container.svelte';
 
 </script>
-<div class="mt-16">
+<div class="mt-16 min-h-screen">
     <Container>
-        <h3 class="text-blue-400">This is contact</h3>
+        <h1 class="pt-16 text-5xl font-normal text-dh-secondary-dark-500 text-center">Thanks for taking the time to reach out. How can I help you today?</h1>
+        <div class="mt-8 flex justify-center">
+            <ContactForm />
+        </div>
     </Container>
 </div>
