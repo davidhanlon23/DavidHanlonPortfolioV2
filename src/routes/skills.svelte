45d101afd796f72ded33b64e58c2476f3a41e129
David Hanlon <davidhanlon23@gmail.com>
2021-10-19 17:55:34 -0400

    clean up skills page

--- src/routes/skills.svelte
+++ src/routes/skills.svelte
@@ -1,109 +1,124 @@
 <script lang="ts">
     import Container from '../components/UI/Container/Container.svelte';
     
-    const iconNameClasses = "text-md font-semibold text-black dark:text-white";
+    const iconNameClasses = "text-md font-semibold text-black dark:text-white flex justify-center";
+    const iconContainerClasses = "my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-12 md:gap-24";
 </script>
 <div class="mt-16">
     <Container>
         <h1 class="pt-16 text-5xl font-normal text-dh-secondary-dark-500 text-center">Technical Skills</h1>
         
+        <!-- Used Most Frequently Section-->
         <h2 class="pt-16 text-lg font-bold text-dh-primary-dark-500 dark:text-white text-center">Used Most Frequently</h2>
         <div class="flex justify-center">
-            <div class="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-1">
-                <i class="devicon-react-original colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> ReactJS </h3>
+            <div class={`${iconContainerClasses}`}>
+                <div class="flex flex-col">
+                    <i class="devicon-react-original colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> ReactJS </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-javascript-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> JavaScript </h3>    
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-nodejs-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> NodeJS </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-tailwindcss-original-wordmark colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> TailwindCSS </h3>
+                </div>  
 
-                <i class="devicon-javascript-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> JavaScript </h3>
-
-                <i class="devicon-nodejs-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> NodeJS </h3>
-
-                <i class="devicon-tailwindcss-original-wordmark colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> TailwindCSS </h3>
-
-
-                <i class="devicon-html5-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> HTML5 </h3>
-
-                <i class="devicon-github-original colored dark:text-white text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Github </h3>
+                <div class="flex flex-col">
+                    <i class="devicon-html5-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> HTML5 </h3>
+                </div>  
+                <div class="flex flex-col">
+                    <i class="devicon-github-original colored dark:text-white text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Github </h3>
+                </div>
 
             </div>
         </div>
 
-
+        <!-- Familiar With Section-->
         <h2 class="pt-16 text-lg font-bold text-dh-primary-dark-500 dark:text-white text-center">Familiar With</h2>
         <div class="flex justify-center">
-            <div class="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-1">
-                <i class="devicon-svelte-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Svelte </h3>
-                <i class="devicon-python-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Python </h3>
-                <i class="devicon-mongodb-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> MongoDB </h3>
-                <i class="devicon-mysql-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> MySQL </h3>
+            <div class={`${iconContainerClasses}`}>
+                <div class="flex flex-col">
+                    <i class="devicon-svelte-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Svelte </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-python-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Python </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-mongodb-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> MongoDB </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-mysql-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> MySQL </h3>
+                </div>
 
-    
-                <i class="devicon-google-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Google App Script </h3>
-                <i class="devicon-drupal-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Drupal CMS </h3>
-                <i class="devicon-java-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Java </h3>
-                <i class="devicon-express-original colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> ExpressJS </h3>
+                <div class="flex flex-col">
+                    <i class="devicon-google-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Google App Script </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-drupal-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Drupal CMS </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-java-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Java </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-express-original colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> ExpressJS </h3>
+                </div>
 
-
-                <i class="devicon-bootstrap-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Boostrap </h3>
-                <i class="devicon-heroku-original colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Heroku </h3>
+                <div class="flex flex-col">
+                    <i class="devicon-bootstrap-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Boostrap </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-heroku-original colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Heroku </h3>
+                </div>
             </div>
         </div>
        
 
-
+        <!-- Future Interests-->
         <h2 class="pt-16 text-lg font-bold text-dh-primary-dark-500 dark:text-white text-center">Future Interests</h2>
         <div class="flex justify-center">
-            <div class="my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-1">
-                <i class="devicon-swift-plain colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Swift </h3>
-                <i class="devicon-go-original-wordmark colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> GoLang </h3>
-                <i class="devicon-rust-plain colored dark:text-white text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> Rust </h3>
-                <i class="devicon-react-original colored text-9xl"></i>
-                <h3 class={`${iconNameClasses}`}> React Native </h3>
-
+            <div class={`${iconContainerClasses}`}>
+                <div class="flex flex-col">
+                    <i class="devicon-go-original-wordmark colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> GoLang </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-rust-plain colored dark:text-white text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Rust </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-react-original colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> React Native </h3>
+                </div>
+                <div class="flex flex-col">
+                    <i class="devicon-swift-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Swift </h3>
+                </div>
             </div>
         </div>
         
 
 
 
-        <!-- 
-            Boostrap
-            Javascript
-
-            MongoDB
-            Heroku 
-            Drupal
-            ExpressJS
-            JAVA
-            Github
+        <!--
             Contentful
-            Svelte
-
-            Google App Script
-            MySQL
-            Python
-
-            Future Interests:
-            React Native
-            
-
         -->
 
     </Container>
