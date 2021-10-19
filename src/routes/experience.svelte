62115e78a38d628c2e0f412588276914901a22a2
David Hanlon <davidhanlon23@gmail.com>
2021-10-19 18:15:47 -0400

    refactor and add to experience section

--- src/routes/experience.svelte
+++ src/routes/experience.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
     import Container from '../components/UI/Container/Container.svelte';
 
+    const secondaryHeaderClasses = "text-black dark:text-white font-semibold text-xl";
+    // const nameOfExperienceClasses = "";
+    // const sectionClasses = "";
 </script>
 <div class="mt-16">
     <Container>
-        <h1 class="pt-16 text-5xl font-normal text-dh-secondary-dark-500 text-center">Job Experience</h1>
+        <h1 class="pt-16 text-5xl font-normal text-dh-secondary-dark-500 text-center">Experience</h1>
+
+        <div class="pt-16">
+            <!-- Work Experience-->
+            <h2 class={`${secondaryHeaderClasses}`}>Work Experience</h2>
+            <div>
+            <!-- MOST RECENT FIRST-->
+
+            </div>
+
+            <!-- Intern Experience Section -->
+            <h2 class={`${secondaryHeaderClasses}`}>Intern Experience</h2>
+            <div>
+
+            </div>
+            
+            <!-- Education Section-->
+            <h2 class={`${secondaryHeaderClasses}`}>Education</h2>
+            <div>
+
+            </div>
+
+        </div>
     </Container>
 </div>
--- src/routes/skills.svelte
+++ src/routes/skills.svelte
@@ -3,37 +3,41 @@
     
     const iconNameClasses = "text-md font-semibold text-black dark:text-white flex justify-center";
     const iconContainerClasses = "my-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-12 md:gap-24";
+    const sectionClasses = "pt-16 text-lg font-bold text-dh-primary-dark-500 dark:text-white text-center";
+    const sectionContainerClasses = "flex justify-center";
+    const iconWrapperClasses = "flex flex-col";
+
 </script>
 <div class="mt-16">
     <Container>
         <h1 class="pt-16 text-5xl font-normal text-dh-secondary-dark-500 text-center">Technical Skills</h1>
         
-        <!-- Used Most Frequently Section-->
-        <h2 class="pt-16 text-lg font-bold text-dh-primary-dark-500 dark:text-white text-center">Used Most Frequently</h2>
-        <div class="flex justify-center">
+        <!-- Used Most Frequently Section -->
+        <h2 class={`${sectionClasses}`}>Used Most Frequently</h2>
+        <div class={`${sectionContainerClasses}`}>
             <div class={`${iconContainerClasses}`}>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-react-original colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> ReactJS </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-javascript-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> JavaScript </h3>    
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-nodejs-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> NodeJS </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-tailwindcss-original-wordmark colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> TailwindCSS </h3>
                 </div>  
 
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-html5-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> HTML5 </h3>
                 </div>  
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-github-original colored dark:text-white text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Github </h3>
                 </div>
@@ -41,49 +45,49 @@
             </div>
         </div>
 
-        <!-- Familiar With Section-->
-        <h2 class="pt-16 text-lg font-bold text-dh-primary-dark-500 dark:text-white text-center">Familiar With</h2>
-        <div class="flex justify-center">
+        <!-- Familiar With Section -->
+        <h2 class={`${sectionClasses}`}>Familiar With</h2>
+        <div class={`${sectionContainerClasses}`}>
             <div class={`${iconContainerClasses}`}>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-svelte-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Svelte </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-python-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Python </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-mongodb-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> MongoDB </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-mysql-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> MySQL </h3>
                 </div>
 
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-google-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Google App Script </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-drupal-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Drupal CMS </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-java-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Java </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-express-original colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> ExpressJS </h3>
                 </div>
 
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-bootstrap-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Boostrap </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-heroku-original colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Heroku </h3>
                 </div>
@@ -91,35 +95,28 @@
         </div>
        
 
-        <!-- Future Interests-->
-        <h2 class="pt-16 text-lg font-bold text-dh-primary-dark-500 dark:text-white text-center">Future Interests</h2>
-        <div class="flex justify-center">
+        <!-- Future Interests Section -->
+        <h2 class={`${sectionClasses}`}>Future Interests</h2>
+        <div class={`${sectionContainerClasses}`}>
             <div class={`${iconContainerClasses}`}>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-go-original-wordmark colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> GoLang </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-rust-plain colored dark:text-white text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Rust </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-react-original colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> React Native </h3>
                 </div>
-                <div class="flex flex-col">
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-swift-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Swift </h3>
                 </div>
             </div>
         </div>
-        
-
-
-
-        <!--
-            Contentful
-        -->
 
     </Container>
 </div>
