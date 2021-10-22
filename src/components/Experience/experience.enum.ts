1ef47ee23e05b18cffa5568793f5b340969c25d4
David Hanlon <davidhanlon23@gmail.com>
2021-10-22 12:53:06 -0400

    stashing changes

--- src/components/Experience/experience.enum.ts
+++ src/components/Experience/experience.enum.ts
@@ -1,119 +1,156 @@
-export const experienceObject = {
-    sections: [
-        {
-            sectionName:"Work Experience",
-            sectionExperience: [
-                {
-                    experienceName: "Cybrary",
-                    experienceWebsite:"https://www.cybrary.it/",
-                    experienceTagline: "Cybrary is an IT/Cybersecurity training and career development platform supporting over 3 million users worldwide.",
-                    experiencePosition: "Frontend Software Engineer",
-                    experienceDates: "September 2020 - Present",
-                    experienceLocation: "College Park, MD",
-                    experienceItems: [
-                        {
-                            experience: "Conducted junior level full-stack (PostgreSQL, Express, ReactJS, NodeJS) development on Stanley Security’s Business to Business & Business to Consumer E-Commerce applications.",
-                        },
-                        {
-                            experience: "Conducted junior level full-stack (PostgreSQL, Express, ReactJS, NodeJS) development on Stanley Security’s Business to Business & Business to Consumer E-Commerce applications.",
-                        },
-                    ],
-                },
-                {
-                    experienceName: "Stanley Black & Decker",
-                    experienceWebsite:"",
-                    experienceTagline: "",
-                    experiencePosition: "",
-                    experienceDates: "",
-                    experienceLocation: "",
-                    experienceItems: [
-                        {
-                            experience: "",
-                        },
-                        {
-                            experience: "",
-                        },
-                    ],
-                },
-            ],
-        },
-        // Internship Experience
-        {
-            sectionName: "Internship Experience",
-            sectionExperience: [
-                {
-                    experienceName: "Cybrary",
-                    experienceWebsite:"",
-                    experienceTagline: "",
-                    experiencePosition: "",
-                    experienceDates: "",
-                    experienceLocation: "",
-                    experienceItems: [
-                        {
-                            experience: "",
-                        },
-                        {
-                            experience: "",
-                        },
-                    ],
-                },
-                {
-                    experienceName: "Electronic Transaction Systems (ETS)",
-                    experienceWebsite:"",
-                    experienceTagline: "",
-                    experiencePosition: "",
-                    experienceDates: "",
-                    experienceLocation: "",
-                    experienceItems: [
-                        {
-                            experience: "",
-                        },
-                        {
-                            experience: "",
-                        },
-                    ],
-                },
-                {
-                    experienceName: "Sprout Creatives",
-                    experienceWebsite:"",
-                    experienceTagline: "",
-                    experiencePosition: "",
-                    experienceDates: "",
-                    experienceLocation: "",
-                    experienceItems: [
-                        {
-                            experience: "",
-                        },
-                        {
-                            experience: "",
-                        },
-                    ],
-                },
-            ],
-        },
-        // Education "experience"
-        // {
-        //     sectionName: "Education",
-        //     sectionExperience: [
-        //         {
-        //             experienceName: "Stanley Black & Decker",
-        //             experienceWebsite:"",
-        //             experienceTagline: "",
-        //             experiencePosition: "",
-        //             experienceDates: "",
-        //             experienceLocation: "",
-        //             experienceItems: [
-        //                 {
-        //                     experience: "",
-        //                 },
-        //                 {
-        //                     experience: "",
-        //                 },
-        //             ],
-        //         },
-        //         {},
-        //     ],
+export const experienceObject = [
+            {
+                sectionName:"Work Experience",
+                sectionExperience: [
+                    {
+                        experienceName: "Cybrary",
+                        experienceNameClasses:"hover:text-green-500 dark:hover:text-green-500",
+                        experienceWebsite:"https://www.cybrary.it/",
+                        experienceTagline: "Cybrary is an IT/Cybersecurity training and career development platform supporting over 3 million users worldwide.",
+                        experiencePosition: "Frontend Software Engineer",
+                        experienceDates: "September 2020 - Present",
+                        experienceLocation: "College Park, MD",
+                        experienceLogo: "/Images/cybrary3.png",
+                        experienceLogoAlt: "Cybrary Logo",
+                        ariaLabel: "Opens Cybrary's site in new tab",
+                        experienceItems: [
+                            {
+                                experience: `Collaborate with team members to coordinate large project releases in a timely manner`,
+                            },
+                            {
+                                experience: `Assisted in the development of multiple B2B and B2C features including the user goals
+                                features for our product application which resulted in improved engagement throughout the
+                                platform and lowering our B2C customer churn rate increasing the company’s overall revenue
+                                and customer retention rate`,
+                            },
+                            {
+                                experience: `Assisted in the entire redesign of the frontend of the product application which provided a
+                                more positive user experience for our 3 million+ users.`,
+                            },
+                            {
+                                experience: `Improve the product application’s accessibility issues which directly resulted in the closing of
+                                multiple B2B deals with various government agencies providing additional long term revenue
+                                streams for the company`,
+                            },
+                            {
+                                experience: `Implement Tailwind CSS framework to replace Semantic UI in order to improve SEO and the
+                                marketing application’s build speed which improved users’ experience and led to marketing
+                                being able to generate more B2C customers.`,
+                            },
+                            {
+                                experience: `Help improve architecture of the marketing application by changing information that was
+                                fetched from our Content Management System (Contentful) during render to being fetched
+                                during build time which resulted in higher seo scores, faster page load times, and improved
+                                user experience`,
+                            },
+                            {
+                                experience: `Fix Cumulative Layout Shift (CLS), First Contentful Paint (FCP) and update our browser polyfill
+                                configurations to improve seo scores.`,
+                            },
+                        ],
+                    },
+                    // {
+                    //     experienceName: "Stanley Black & Decker",
+                    //     experienceWebsite:"",
+                    //     experienceTagline: "",
+                    //     experiencePosition: "",
+                    //     experienceDates: "",
+                    //     experienceLocation: "",
+                    //     experienceItems: [
+                    //         {
+                    //             experience: "",
+                    //         },
+                    //         {
+                    //             experience: "",
+                    //         },
+                    //     ],
+                    // },
+                ],
+            },
+            // Internship Experience
+            // { 
+            //     sectionName: "Internship Experience",
+            //     sectionExperience: [
+            //         {
+            //             experienceName: "Cybrary",
+            //             experienceWebsite:"",
+            //             experienceTagline: "",
+            //             experiencePosition: "",
+            //             experienceDates: "",
+            //             experienceLocation: "",
+            //             experienceItems: [
+            //                 {
+            //                     experience: "",
+            //                 },
+            //                 {
+            //                     experience: "",
+            //                 },
+            //             ],
+            //         },
+            //         {
+            //             experienceName: "Electronic Transaction Systems (ETS)",
+            //             experienceWebsite:"",
+            //             experienceTagline: "",
+            //             experiencePosition: "",
+            //             experienceDates: "",
+            //             experienceLocation: "",
+            //             experienceItems: [
+            //                 {
+            //                     experience: "",
+            //                 },
+            //                 {
+            //                     experience: "",
+            //                 },
+            //             ],
+            //         },
+            //         {
+            //             experienceName: "Sprout Creatives",
+            //             experienceWebsite:"",
+            //             experienceTagline: "",
+            //             experiencePosition: "",
+            //             experienceDates: "",
+            //             experienceLocation: "",
+            //             experienceItems: [
+            //                 {
+            //                     experience: "",
+            //                 },
+            //                 {
+            //                     experience: "",
+            //                 },
+            //             ],
+            //         },
+            //     ],
+            // },
+            // Education "experience"
+            // {
+            //     sectionName: "Education",
+            //     sectionExperience: [
+            //         {
+            //             experienceName: "Stanley Black & Decker",
+            //             experienceWebsite:"",
+            //             experienceTagline: "",
+            //             experiencePosition: "",
+            //             experienceDates: "",
+            //             experienceLocation: "",
+            //             experienceItems: [
+            //                 {
+            //                     experience: "",
+            //                 },
+            //                 {
+            //                     experience: "",
+            //                 },
+            //             ],
+            //         },
+            //         {},
+            //     ],
+            // },
+    
+        // experienceClasses: {
+        //     secondaryHeaderClasses: "text-black dark:text-white font-semibold text-xl",
+        //     nameOfExperienceClasses: "text-black dark:text-white text-4xl",
+        //     taglineClasses: "italic",
+    
+            
         // },
-    ],
-};
+];
   
