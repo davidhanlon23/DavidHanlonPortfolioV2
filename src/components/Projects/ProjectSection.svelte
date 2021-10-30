b339840729d2085df7ee961c8dacf41e60064145
David Hanlon <davidhanlon23@gmail.com>
2021-10-30 15:15:08 -0400

    updates to project page

--- src/components/Projects/ProjectSection.svelte
+++ src/components/Projects/ProjectSection.svelte
@@ -11,28 +11,15 @@
         projectRepoLink: '',
         projectDescription: ``,
         projectImageUrl: '',
+        projectLabelYear: '',
     },
     ];
-    /*
-        vertical-align: middle;
-        margin-left: 5px;
-
-        color: #fff;
-        background-color: #5b43d6;
-
-            display: inline-block;
-            padding: .25em .4em;
-            font-weight: 700;
-            text-align: center;
-            white-space: nowrap;
-            border-radius: .25rem;
-    */
 
     const ctaClasses = "w-full mx-auto mb-4 sm:mx-0 sm:mb-0 sm:w-auto flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm  sm:px-8";
 </script>
 {#each projects as project}
 <Container>
-    <div class="flex flex-wrap justify-center flex-col md:flex-row mb-8 md:w-full">
+    <div class="flex flex-wrap justify-center flex-col md:flex-row my-16 md:w-full">
         <div class="md:max-w-md md:mt-2">
             <!-- IMAGE GOES HERE -->
             <Image src={project.projectImageUrl} />
@@ -40,17 +27,17 @@
         <div class="mt-8 md:mt-0 md:max-w-md mx-auto">
             <div class="flex mb-12  text-center justify-center md:justify-start">
                 <h2 class="text-3xl font-bold text-black dark:text-white">{project.projectName}</h2>
-                <Label classes="my-auto items-center" value="2020"/>
+                <Label classes="my-auto items-center" value={project.projectLabelYear} />
             </div>
             <div>
                 <p class="text-black dark:text-white text-center md:text-left">{project.projectDescription}</p>
             </div>
             <div class={`flex my-8 ${project.projectWebsite && project.projectRepoLink ? 'flex-col md:flex-row': 'flex-row'}`}>
                 {#if project.projectWebsite}
-                    <Button classes={`${ctaClasses} ${project.projectRepoLink ? 'md:mr-8':''}`} text="Visit Website" color="primary" />
+                    <Button classes={`${ctaClasses} ${project.projectRepoLink ? 'md:mr-8':''}`} text="Visit Website" color="primary" href={`${project.projectWebsite}`} accessibilityProps={{'aria-label':`Open ${project.projectName} in a new tab`}} target="_blank" />
                 {/if}
                 {#if project.projectRepoLink}
-                    <Button classes={`${ctaClasses}`} text="Visit Repo" color="secondary" />
+                    <Button classes={`${ctaClasses}`} text="Visit Repo" color="secondary" href={`${project.projectRepoLink}`} accessibilityProps={{'aria-label':`Open ${project.projectName}'s repo in a new tab`}} target="_blank" />
                 {/if}
             </div>
         </div>
