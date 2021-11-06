a0661ad0feb78be44f8aa1c9d239236cc53af79d
David Hanlon <davidhanlon23@gmail.com>
2021-11-06 16:05:24 -0400

    changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,31 @@
 import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true
+    })
+  ],
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			optimizeDeps: {
-				exclude: ['@urql/svelte']
-			}
-		}
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      optimizeDeps: {
+        exclude: ['@urql/svelte']
+      },
+    },
+    adapter: adapter({
+      // default options are shown
+      pages: 'build',
+      assets: 'build',
+      fallback: null
+    })
+  }
 }
 
 export default config
