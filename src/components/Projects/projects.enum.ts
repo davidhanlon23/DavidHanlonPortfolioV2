82cd51a0d631dcdbd9545357c1a411144b94c5d7
David Hanlon <davidhanlon23@gmail.com>
2021-11-06 19:20:38 -0400

    comment code

--- src/components/Projects/projects.enum.ts
+++ src/components/Projects/projects.enum.ts
@@ -1,13 +1,13 @@
 export const projectsObject = [
+    // {
+    //     projectName: 'Test Project',
+    //     projectWebsite: 'https://www.google.com',
+    //     projectRepoLink: 'https://www.github.com/',
+    //     projectDescription: `Lorem ipsum dolor sit amet, vitae porta sapien imperdiet at. Morbi massa felis, lacinia ac purus et, eleifend imperdiet ligula. Phasellus accumsan ante eros, a tincidunt justo tempus eu. Integer egestas fermentum imperdiet. Nulla eget massa ex. Vivamus dapibus justo consequat neque aliquam, et mattis leo facilisis. Nam lacus est, sagittis congue mi ac, accumsan ultricies ligula. Ut semper, justo nec laoreet dapibus, diam ligula porta elit, vitae dictum arcu eros id dui. Ut ac sem interdum, porta magna vitae`, 
+    //     projectImageUrl: '/Images/needToEdit.png',
+    //     projectLabelYear: '2021',
+    // },
     {
-        projectName: 'Test Project',
-        projectWebsite: 'https://www.google.com',
-        projectRepoLink: 'https://www.github.com/',
-        projectDescription: `Lorem ipsum dolor sit amet, vitae porta sapien imperdiet at. Morbi massa felis, lacinia ac purus et, eleifend imperdiet ligula. Phasellus accumsan ante eros, a tincidunt justo tempus eu. Integer egestas fermentum imperdiet. Nulla eget massa ex. Vivamus dapibus justo consequat neque aliquam, et mattis leo facilisis. Nam lacus est, sagittis congue mi ac, accumsan ultricies ligula. Ut semper, justo nec laoreet dapibus, diam ligula porta elit, vitae dictum arcu eros id dui. Ut ac sem interdum, porta magna vitae`, 
-        projectImageUrl: '/Images/needToEdit.png',
-        projectLabelYear: '2021',
-    },
-       {
         projectName: 'Endeavors.tv',
         projectWebsite: 'https://Endeavors.tv',
         projectRepoLink: '',
