c4efbe2e0a0af67fcc5d527b982ccc61cb6c3827
David Hanlon <davidhanlon23@gmail.com>
2022-08-16 14:53:07 -0400

    update skills

--- src/routes/skills.svelte
+++ src/routes/skills.svelte
@@ -25,6 +25,10 @@
                     <h3 class={`${iconNameClasses}`}> JavaScript </h3>    
                 </div>
                 <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-typescript-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> TypeScript </h3>    
+                </div>
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-nodejs-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> NodeJS </h3>
                 </div>
@@ -32,16 +36,27 @@
                     <i class="devicon-tailwindcss-original-wordmark colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> TailwindCSS </h3>
                 </div>  
-
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-gatsby-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Gatsby </h3>
+                </div>
+               
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-vscode-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> VsCode </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-github-original colored dark:text-white text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Github </h3>
+                </div>
                 <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-html5-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> HTML5 </h3>
                 </div>  
                 <div class={`${iconWrapperClasses}`}>
-                    <i class="devicon-github-original colored dark:text-white text-9xl"></i>
-                    <h3 class={`${iconNameClasses}`}> Github </h3>
-                </div>
-
+                    <i class="devicon-eslint-original colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> ESLint </h3>
+                </div>  
             </div>
         </div>
 
@@ -50,12 +65,68 @@
         <div class={`${sectionContainerClasses}`}>
             <div class={`${iconContainerClasses}`}>
                 <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-solidity-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Solidity </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-polygon-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Polygon </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-nextjs-original colored text-9xl dark:text-white"></i>
+                    <h3 class={`${iconNameClasses}`}> NextJS </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-mocha-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Mocha </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-graphql-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> GraphQL </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-digitalocean-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Digital Ocean </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-amazonwebservices-plain-wordmark colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> AWS Amplify </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-react-original colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> React Native </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-firebase-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Firebase </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-svelte-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Svelte </h3>
                 </div>
                 <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-discordjs-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> DiscordJS </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-materialui-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Material UI </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-npm-original-wordmark colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> NPM </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-webpack-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Webpack </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-python-plain colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Python </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-postgresql-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Postgres </h3>
                 </div>
                 <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-mongodb-plain colored text-9xl"></i>
@@ -79,6 +150,10 @@
                     <h3 class={`${iconNameClasses}`}> Java </h3>
                 </div>
                 <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-ionic-original colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Ionic Framework </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-express-original colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> ExpressJS </h3>
                 </div>
@@ -90,6 +165,14 @@
                 <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-heroku-original colored text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Heroku </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-figma-plain colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Figma </h3>
+                </div>
+                <div class={`${iconWrapperClasses}`}>
+                    <i class="devicon-sketch-line colored text-9xl"></i>
+                    <h3 class={`${iconNameClasses}`}> Sketch UI </h3>
                 </div>
             </div>
         </div>
@@ -106,10 +189,6 @@
                 <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-rust-plain colored dark:text-white text-9xl"></i>
                     <h3 class={`${iconNameClasses}`}> Rust </h3>
-                </div>
-                <div class={`${iconWrapperClasses}`}>
-                    <i class="devicon-react-original colored text-9xl"></i>
-                    <h3 class={`${iconNameClasses}`}> React Native </h3>
                 </div>
                 <div class={`${iconWrapperClasses}`}>
                     <i class="devicon-swift-plain colored text-9xl"></i>
